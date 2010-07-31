a94c10a7cbd4b3472296c02e5ac395c5d390ea9d
Guillaume ALAUX <guillaume@alaux.net>
2010-07-31 15:49:26 +0200

    Updated tomcat7 to mirror tomcat changes

--- tomcat7/tomcat7.conf.d
+++ tomcat7/tomcat7.conf.d
@@ -1,4 +1,5 @@
 # Tomcat additional commandline options
 CATALINA_OPTS=
 
-ERROR_FILE=/var/log/tomcat7/catalina.log
+OUT_FILE=/var/log/tomcat7/catalina-out.log
+ERROR_FILE=/var/log/tomcat7/catalina-err.log
